2f44f62d1e5013fe9d92a62b3a56cba677aae965
BegoCaZa <begoxcz@hotmail.com>
2025-03-17 11:38:28 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// let user
+// user='pepe'
+
+// let numberA = 89
+
+// const numberB= 64
+
+// console.log(numberA)
+// numberA= 12
+
+// console.log(numberA)
+
+// function printOne(){
+//     console.log(1);
+// }
+
+// function printTwo(){
+//     console.log(2);
+// }
+
+// function printThree(){
+//     console.log(3);
+// }
+
+// printOne();
+// printTwo();
+// printThree();
+
+function printNumber(cosa){
+    console.log(cosa +1);
+}
+
+printNumber('1');
+printNumber(2);
+printNumber(3);
